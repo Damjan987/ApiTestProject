40f15608fec2e0d553f5a086dbc7a3efc1606122
Damjan987 <jasamprogramer7@gmail.com>
2022-04-09 22:22:44 +0200

    Add project files.

---
+++ ApiTest.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiTest.WebUI.MvcApplication" Language="C#" %>
---
+++ ApiTest.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApiTest.WebUI.Startup))]
+namespace ApiTest.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
